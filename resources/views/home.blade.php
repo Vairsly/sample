3233ffcc959db0a210b2340e73efda9edd868d60
Vairsly <Vairsly@foxmail.com>
2019-02-16 03:58:50 +0000

    Finish static pages

--- resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-    <title>Sample App</title>
-</head>
-<body>
-<h1>主页</h1>
-</body>
-</html>
-
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>@yield('title','Sample')</title>
+<body>
+    @yield('content')
+</body>
+</head>
+</html>
